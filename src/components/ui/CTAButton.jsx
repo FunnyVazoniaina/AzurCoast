6d7e47ceb4d39cc84f46d536bb81e82745503055
FunnyVazoniaina <funnyvazoniaina@gmail.com>
2025-09-23 11:08:48 +0300

    style: change icon properly

--- src/components/ui/CTAButton.jsx
+++ src/components/ui/CTAButton.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import { ArrowRight } from "lucide-react";
+import { ChevronRightCircle } from "lucide-react";
 
 export default function CTAButton({ text, onClick }) {
   return (
@@ -8,7 +8,7 @@ export default function CTAButton({ text, onClick }) {
       className="mt-12 inline-flex items-center gap-3 px-6 py-3 border border-white text-white font-semibold rounded-full hover:bg-white hover:text-black transition-colors shadow-md"
     >
       {text}
-      <ArrowRight size={20} />
+      <ChevronRightCircle size={22} />
     </button>
   );
 }
