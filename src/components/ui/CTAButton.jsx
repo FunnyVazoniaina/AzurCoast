edafc350ce663d0d85e3416d839653c72afd163b
FunnyVazoniaina <funnyvazoniaina@gmail.com>
2025-09-23 14:27:46 +0300

    fix: edit generic button to accept classname params

--- src/components/ui/CTAButton.jsx
+++ src/components/ui/CTAButton.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 import { ChevronRightCircle } from "lucide-react";
 
-export default function CTAButton({ text, onClick }) {
+export default function CTAButton({ text, onClick, className = "" }) {
   return (
     <button
       onClick={onClick}
-      className="mt-12 inline-flex items-center gap-3 px-6 py-3 border border-white text-white font-semibold rounded-full hover:bg-white hover:text-black transition-colors shadow-md"
+      className={`mt-12 inline-flex items-center gap-3 px-6 py-3 border-2 font-semibold rounded-full transition-colors shadow-md ${className}`}
     >
       {text}
       <ChevronRightCircle size={22} />
